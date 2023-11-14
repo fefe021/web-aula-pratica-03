3a5c19aed1f403b2591425eb0485ac59361e26d5
joao <joao@hotmail.com>
2023-11-14 11:11:32 -0300

    modelo estudante

--- studentManagement/models.py
+++ studentManagement/models.py
@@ -7,3 +7,6 @@ class Student(models.Model):
     Classe responsável pelo cadastro de alunos
     """
     name = models.CharField(max_length=50, blank=False)
+    idade = models.CharField(max_length=2, blank=False, default="")
+    email = models.CharField(max_length=20, blank=False, default="")
+    
